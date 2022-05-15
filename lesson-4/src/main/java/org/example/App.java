6ec083a148f2a3c52519dd13cfabc9b33518c3ae
vanyakulyk <ivan.kulyk.1996@gmail.com>
2022-05-15 19:24:06 +0300

    custom HashTable

---
+++ lesson-4/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class App {
+    public static void main( String[] args ) {
+
+        HashTable<String, String> hashTable = new HashTable<>();
+
+        hashTable.put("one", "Dima");
+        hashTable.put("two", "Masha");
+        hashTable.put("three", "Kostya");
+        hashTable.put("four", "Rita");
+
+        hashTable.printTable();
+    }
+}
